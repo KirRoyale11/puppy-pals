6ad86c07d7b3a354d0376f09b423cb7a3d212a32
Kirsti Esch <miss.k.esch@gmail.com>
2024-07-10 11:57:01 -0400

    Functionality add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { puppyList } from "./data.js";
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((puppy) => puppy.id === featPupId);
 
   return (
     <>
-     APP
+    {featPupId && (
+      <div>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+      </div>
+    ) }
+      {puppies.map((puppy) => (
+        <p
+          key={puppy.id}
+          onClick={() => {
+            setFeatPupId(puppy.id);
+          }}
+        >
+          {puppy.name}
+        </p>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
